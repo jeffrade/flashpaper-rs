675f82ce872fafb08ccd5c76171ab2598c0b2b9f
Jeff Rade <jeffrade@gmail.com>
2022-05-01 19:34:17 -0500

    Add Copy Url button; Remove Home href

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,8 @@
-// Intentionally left blank
+function copyToClipboard() {
+    var urlText = document.getElementById("one_time_url");
+    var CopyRange = document.createRange();
+    CopyRange.selectNode(urlText);
+    window.getSelection().addRange(CopyRange);
+    document.execCommand("copy");
+    window.getSelection().removeRange(CopyRange);
+}
